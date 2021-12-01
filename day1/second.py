66d565c2a573e538ad77200c0fd72df77fed3959
Kiley Poole <kpoole007@gmail.com>
2021-12-01 08:53:26 -0800

    cleaned up second solution day 1

--- day1/second.py
+++ day1/second.py
@@ -6,16 +6,14 @@ total_increase = 0
 for line in f:
     nums.append(int(line))
 
-first_start, first_end = 0, 2
-second_start, second_end = 1, 3
+first_start = 0
+second_end =  3
 
 while second_end <= len(nums) - 1:
-    first_sum = nums[first_start] + nums[first_end] + nums[second_start]
-    second_sum = nums[second_start] + nums[second_end] + nums[first_end]
-    if first_sum < second_sum:
+    if nums[first_start]  < nums[second_end]:
         total_increase += 1
     
-    first_start, first_end = second_start, second_end
-    second_start, second_end = second_start + 1, second_end + 1
+    first_start = first_start + 1
+    second_end = second_end + 1
 
 print(total_increase)
